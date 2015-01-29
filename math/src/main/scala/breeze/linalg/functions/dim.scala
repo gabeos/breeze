8568884acc0527171a06fe7e660f874d50544176
Gabriel Schubiner <gabeos@cs.washington.edu>
2015-01-28 21:53:59 -0800

    encoder updates

--- math/src/main/scala/breeze/linalg/functions/dim.scala
+++ math/src/main/scala/breeze/linalg/functions/dim.scala
@@ -6,15 +6,13 @@ import breeze.generic.UFunc
  * breeze
  * 7/15/14
  * @author Gabriel Schubiner <gabeos@cs.washington.edu>
- *
- *
  */
 object dim extends UFunc {
-  implicit def implVDim[V <:Tensor[_,_]]: Impl[V,Int] = new Impl[V,Int] {
+  implicit def implVDim[V <:Vector[_]]: Impl[V,Int] = new Impl[V,Int] {
     override def apply(v: V): Int = v.size
   }
 
-  implicit def implMDim[T, M <: Matrix[T]]: Impl[M,(Int,Int)] = new Impl[M,(Int,Int)] {
+  implicit def implMDim[M <: Matrix[_]]: Impl[M,(Int,Int)] = new Impl[M,(Int,Int)] {
     override def apply(v: M): (Int, Int) = (v.rows,v.cols)
   }
 }
--- math/src/main/scala/breeze/util/Encoder.scala
+++ math/src/main/scala/breeze/util/Encoder.scala
@@ -2,8 +2,10 @@ package breeze.util
 
 import breeze.linalg._
 import breeze.collection.mutable._
+import breeze.math.Semiring
 import breeze.storage._
 import java.util
+import scala.{specialized=>spec}
 import scala.reflect.ClassTag
 
 /**
@@ -17,50 +19,59 @@ trait Encoder[T] {
   /**
    * Creates a SparseVector[Double] with the index's size
    */
-  def mkSparseVector(): SparseVector[Double] = {
-    SparseVector.zeros[Double](index.size)
+  def mkSparseVector[@spec(Double,Int,Float,Long) S:Zero:ClassTag](): SparseVector[S] = {
+    SparseVector.zeros[S](index.size)
   }
 
   /**
    * Creates a DenseVector[Double] with the index's size
    */
-  final def mkDenseVector(default: Double=0.0):DenseVector[Double] = {
-    val array = new Array[Double](index.size)
-    util.Arrays.fill(array, default)
+  final def mkDenseVector[@spec(Double,Int,Float,Long) S:Semiring:ClassTag](default: S): DenseVector[S] = {
+    val array = Array.fill(index.size)(default)
     new DenseVector(array)
   }
+
+  final def mkDenseVector[@spec(Double,Int,Float,Long) S:Semiring:ClassTag](): DenseVector[S] = mkDenseVector[S](implicitly[Semiring[S]].zero)
 
   /**
    * Creates a Vector[Double] of some sort with the index's size.
    */
-  final def mkVector():Vector[Double] = mkSparseVector()
+  final def mkVector[@spec(Double,Int,Float,Long) S:Zero:ClassTag]():Vector[S] = mkSparseVector[S]()
 
   /**
    * makes a matrix of some sort with the index's size as rows and cols
    */
-  final def mkMatrix():DenseMatrix[Double] = {
-    DenseMatrix.zeros(index.size,index.size)
+  final def mkMatrix[@spec(Double,Int,Float,Long) S:Zero:ClassTag]():DenseMatrix[S] = {
+    DenseMatrix.zeros[S](index.size,index.size)
   }
 
   /**
    * Decodes a vector back to a Counter[T,Double]
    */
-  def decode(v: Vector[Double], keepZeros: Boolean = false):Counter[T,Double] = {
-    val ctr = Counter[T,Double]()
+//  def decode(v: Vector[Double], keepZeros: Boolean = false):Counter[T,Double] = {
+//    val ctr = Counter[T,Double]()
+//    for( (i,v) <- v.active.pairs) {
+//      if(keepZeros || v != 0.0)
+//        ctr(index.get(i)) = v
+//    }
+//    ctr
+//  }
+
+  def decode[@spec(Double,Int,Float,Long) S:Zero:Semiring](v: Vector[S], keepZeros: Boolean = false): Counter[T,S] = {
+    val ctr = Counter[T,S]()
     for( (i,v) <- v.active.pairs) {
-      if(keepZeros || v != 0.0)
+      if(keepZeros || v != implicitly[Zero[S]].zero)
         ctr(index.get(i)) = v
     }
     ctr
   }
 
-
   /**
    * Encodes a DoubleCounter as a Vector[Double].
    * All elements in the counter must be in the index unless ignoreOutOfIndex is true
    */
-  def encodeDense(c: Tensor[T,Double], ignoreOutOfIndex:Boolean = false):DenseVector[Double] = {
-    val vec = mkDenseVector()
+  def encodeDense[@spec(Double,Int,Float,Long) S:Zero:Semiring:ClassTag](c: Tensor[T,S], ignoreOutOfIndex:Boolean = false):DenseVector[S] = {
+    val vec = mkDenseVector[S]()
     for( (k,v) <- c.active.pairs) {
       val ki = index(k)
       if(ki < 0) {if(!ignoreOutOfIndex) throw new RuntimeException("Error, not in index: " + k)}
@@ -73,8 +84,8 @@ trait Encoder[T] {
    * Encodes a DoubleCounter as a SparseVector[Double].
    * All elements in the counter must be in the index unless ignoreOutOfIndex is true
    */
-  def encodeSparse(c: Tensor[T,Double], ignoreOutOfIndex: Boolean = false):SparseVector[Double] = {
-    val vec = new VectorBuilder[Double](index.size)
+  def encodeSparse[@spec(Double,Int,Float,Long) S:Zero:Semiring:ClassTag](c: Tensor[T,S], ignoreOutOfIndex: Boolean = false):SparseVector[S] = {
+    val vec = new VectorBuilder[S](index.size)
     vec.reserve(c.activeSize)
     for( (k,v) <- c.active.pairs) {
       val ki = index(k)
@@ -88,8 +99,8 @@ trait Encoder[T] {
    * Encodes a DoubleCounter as a Vector[Double].
    * All elements in the counter must be in the index unless ignoreOutOfIndex is true
    */
-  def encode(c: Tensor[T,Double], ignoreOutOfIndex: Boolean = false):Vector[Double] = {
-    val vec = mkVector()
+  def encode[@spec(Double,Int,Float,Long) S:Zero:ClassTag](c: Tensor[T,S], ignoreOutOfIndex: Boolean = false):Vector[S] = {
+    val vec = mkVector[S]()
     for( (k,v) <- c.active.pairs) {
       val ki = index(k)
       if(ki < 0) {if(!ignoreOutOfIndex) throw new RuntimeException("Error, not in index: " + k)}
@@ -102,8 +113,8 @@ trait Encoder[T] {
    * Encodes a Tensor[(T,T),Double] as a DenseMatrix[Double].
    * All elements in the counter must be in the index unless ignoreOutOfIndex is true
    */
-  def encode(c: Tensor[(T,T),Double]):DenseMatrix[Double] = {
-    val vec = mkMatrix()
+  def encode[@spec(Double,Int,Float,Long) S:Zero:ClassTag](c: Tensor[(T,T),S]):DenseMatrix[S] = {
+    val vec = mkMatrix[S]()
     for( ((k,l),v) <- c.active.pairs) {
       val ki = index(k)
       val li = index(l)
@@ -118,17 +129,17 @@ trait Encoder[T] {
   /**
    * Creates an array of arbitrary type with the index's size.
    */
-  def mkArray[V:ClassTag] = new Array[V](index.size)
+  def mkArray[@spec(Double,Int,Float,Long) V:ClassTag] = new Array[V](index.size)
 
   /**
    * Fills an array of arbitrary type with the value provided and with the index's size.
    */
-  def fillArray[V:ClassTag](default : => V): Array[V] = Array.fill(index.size)(default)
+  def fillArray[@spec(Double,Int,Float,Long) V:ClassTag](default : => V): Array[V] = Array.fill(index.size)(default)
 
   /**
    * Fills an array of arbitrary type by tabulating the function
    */
-  def tabulateArray[V:ClassTag](f: T=>V): Array[V] = {
+  def tabulateArray[@spec(Double,Int,Float,Long) V:ClassTag](f: T=>V): Array[V] = {
     val arr = new Array[V](index.size)
     for((e,i) <- index.pairs) {
       arr(i) = f(e)
@@ -139,23 +150,21 @@ trait Encoder[T] {
   /**
    * Fills a DenseVector[Double] with each index given by the result of the function.
    */
-  def tabulateDenseVector(f: T=>Double)  = new DenseVector[Double](tabulateArray[Double](f))
+  def tabulateDenseVector[@spec(Double,Int,Float,Long) S:ClassTag](f: T=>S)  = new DenseVector[S](tabulateArray[S](f))
 
   /**
    * Converts an array into a Map from T's to whatever was in the array.
    */
-  def decode[V](array: Array[V]):Map[T,V] = {
+  def decode[@spec(Double,Int,Float,Long) V](array: Array[V]):Map[T,V] = {
     Map.empty ++ array.zipWithIndex.map{ case (v,i) => (index.get(i),v)}
   }
 
-  def fillSparseArrayMap[V:ClassTag:Zero](default: =>V) = new SparseArrayMap[V](index.size, default)
+  def fillSparseArrayMap[@spec(Double,Int,Float,Long) V:ClassTag:Zero](default: =>V) = new SparseArrayMap[V](index.size, default)
 
-  def mkSparseArray[V:ClassTag:Zero] = new SparseArray[V](index.size)
-  def decode[V](array: SparseArray[V]):Map[T,V] = {
+  def mkSparseArray[@spec(Double,Int,Float,Long) V:ClassTag:Zero] = new SparseArray[V](index.size)
+  def decode[@spec(Double,Int,Float,Long) V](array: SparseArray[V]):Map[T,V] = {
     Map.empty ++ array.iterator.map{ case (i,v) => (index.get(i),v)}
   }
-
-
 }
 
 object Encoder {
